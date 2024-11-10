3222c4a769edab7eda7f3b32c4f7d608f5b28842
bluebanana21 <m.affanfahrozi@gmail.com>
2024-11-10 17:36:02 +0700

    update: nabvar pushes instead of switching

--- lib/main.dart
+++ lib/main.dart
@@ -5,66 +5,76 @@ import 'package:kantin_app/pages/cart_page.dart';
 import 'package:kantin_app/pages/home_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+const supabaseUrl = 'https://akrzqrmfgsvgqgpczwiw.supabase.co';
+const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
+
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Supabase.initialize(
-    url: 'https://akrzqrmfgsvgqgpczwiw.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrcnpxcm1mZ3N2Z3FncGN6d2l3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0Njc5MTgsImV4cCI6MjA0NjA0MzkxOH0.RpKztdynh9WsvygHthzYGEkmY3jdywEuoO0jULaSC-8',
-  );
-
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
   runApp(const MainApp());
 }
 
 final SupabaseClient supabase = Supabase.instance.client;
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
-  @override
-  _MainAppState createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _pages = [
-    const HomePage(),
-    const CartPage(),
-    const AdminFoodPage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: _pages[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart),
-              label: 'Cart',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_chart),
-              label: 'Chart',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  void _onItemPressed(int index) {
+    Widget page;
+
+    switch (index) {
+      case 0:
+        page = const HomePage();
+        break;
+      case 1:
+        page = const CartPage();
+        break;
+      case 2:
+        page = const AdminFoodPage();
+        break;
+      default:
+        page = const HomePage();
+    }
+
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => page));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const HomePage(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_chart),
+            label: 'Admin',
+          ),
+        ],
+        onTap: _onItemPressed,
       ),
     );
   }
